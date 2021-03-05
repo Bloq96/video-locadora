d12f21c6bc8a2a0dc0d3ec183dab9d0d0782ac0f
Gabriel <grt.araujo@hotmail.com>
2021-03-05 14:05:59 -0300

    Commit 6

--- Rental.java
+++ Rental.java
@@ -34,5 +34,14 @@ public class Rental {
             break;
       }
       return thisAmount;
-  }
+   }
+
+   public int getFrequentRenterPoints() {
+      // add bonus for a two day new release rental
+      if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+         getDaysRented() > 1)
+         return 2;
+      else
+         return 1;
+   }
 }
